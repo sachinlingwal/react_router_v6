7f112a59739591ff4cc76190053430d7b55c9166
sachinlingwal <sachinlingwal48@gmail.com>
2022-03-30 09:33:03 +0530

    Navlink and active link color chnage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Navbar = () => {
+  const ActiveLink = ({ isActive }) => {
+    return { color: isActive ? "red" : "", margin: "1em" };
+  };
   return (
     <div>
-      {/* <a href="/">Home</a>
-      <a href="/about">About</a> */}
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="about">About</Link>
-      <br />
-      <Link to="about/123">About US ID</Link>
-      <br />
-
+      <NavLink to="/" style={ActiveLink}>
+        Home
+      </NavLink>
+      <NavLink style={ActiveLink} to="about">
+        About
+      </NavLink>
+      <NavLink style={ActiveLink} to="about/123">
+        About US ID
+      </NavLink>
       {/* we can send state  */}
-      <Link to="contact" state={{ name: "ReactJs" }}>
+      <NavLink style={ActiveLink} to="contact" state={{ name: "ReactJs" }}>
         Contact us
-      </Link>
-      <br />
+      </NavLink>
       <div style={{ margin: "2em 1em" }}>
         <Outlet />
       </div>
